6f089837db18884fb668f64519ccde0e2bb2d12a
karla buckner and Rebecca McIntosh <karla@>
2017-12-04 11:38:06 -0800

    add user auth, navbar and some styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def authorize
+    if !current_user
+      flash[:alert] = "Please sign in to visit this page."
+      redirect_to '/'
+    end
+  end
+
+  def authorize_admin
+    if current_user.admin === true
+      new_cv_path
+    else
+      flash[:alert] = "Only an admin can visit this page."
+      redirect_to '/'
+    end
+  end
+
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :authorize, :except => [:index]
+
   def index
     @products = Product.all
   end
